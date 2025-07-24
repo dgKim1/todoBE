f023f250ccc671961cc83edb48c01f8ca45e0475
dgKim1 <kimdg3556@naver.com>
2025-07-24 13:13:55 +0900

    fix: cors config 설정

--- app.js
+++ app.js
@@ -1,11 +1,21 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 const bodyParser = require("body-parser");
 const indexRouter = require("./routes/index");
 const app = express();
+require("dotenv").config();
+
+const mongoURI = process.env.MONGODB_URI_PROD;
+app.use(
+  cors({
+    origin: "http://localhost:3000", // 개발용 프론트 주소만 허용
+    credentials: true,
+  })
+);
+
 app.use(bodyParser.json());
 app.use("/api", indexRouter);
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
 
 mongoose
   .connect(mongoURI, { useNewUrlParser: true })
@@ -16,6 +26,6 @@ mongoose
     console.log("DB connnection failed", err);
   });
 
-app.listen(5000, () => {
-  console.log("server on 5000");
+app.listen(process.env.PORT || 5000, () => {
+  console.log(`server on ${process.env.PORT}`);
 });
